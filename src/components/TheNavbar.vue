6736e2cd43294017a7015395403314bb9b2c62e3
Maciej Romanowski <neipreis22@gmail.com>
2021-12-11 00:05:48 +0100

    created navbar

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app-bar
+    app
+    color="primary"
+    dark
+  >
+    <div class="d-flex align-center">
+      <v-img
+        alt="Vuetify Logo"
+        class="shrink mr-3"
+        contain
+        src="../assets/logo2.png"
+        transition="scale-transition"
+        width="40"
+      />
+
+      Bestiariusz miejski
+    </div>
+
+    <v-spacer />
+
+    <!-- user -->
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'TheNavbar',
+};
+</script>
