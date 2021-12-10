33913a546f647811938b5e6f74ae9e9edd497785
Maciej Romanowski <neipreis22@gmail.com>
2021-12-11 00:05:43 +0100

    created bottom navbar

---
+++ src/components/TheBottomNavbar.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-bottom-navigation
+    v-model="navigation"
+    app
+    grow
+    color="primary"
+    background-color="secondary"
+    class="elevation-1"
+    mandatory
+  >
+    <v-btn :to="{ name: 'Cities' }">
+      <span>
+        Miasto
+      </span>
+      <v-icon>
+        mdi-city-variant
+      </v-icon>
+    </v-btn>
+    <v-btn :to="{ name: 'Map' }">
+      <span>
+        Mapa
+      </span>
+      <v-icon>
+        mdi-earth
+      </v-icon>
+    </v-btn>
+    <v-btn :to="{ name: 'Carts' }">
+      <span>
+        Karty
+      </span>
+      <v-icon>
+        mdi-cards
+      </v-icon>
+    </v-btn>
+    <v-btn :to="{ name: 'Report' }">
+      <span>
+        Zgłoś
+      </span>
+      <v-icon>
+        mdi-camera-marker
+      </v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+
+<script>
+export default {
+  name: 'TheBottomNavbar',
+  data: () => ({
+    navigation: 0,
+  }),
+};
+</script>
