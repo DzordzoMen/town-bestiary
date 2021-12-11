e1fd8fd719df28683847763ed1dcc6126a2dab07
Maciej Romanowski <neipreis22@gmail.com>
2021-12-11 05:49:06 +0100

    added pins to map and areas

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -6,15 +6,74 @@
     :center="center"
   >
     <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-    <l-marker :lat-lng="markerLatLng"></l-marker>
+    <l-marker
+      :lat-lng="[marker.longitude, marker.latitude]"
+      :name="marker.name"
+      v-for="(marker, index) in markers"
+      :key="index"
+    >
+      <l-tooltip>
+          {{ marker.name }}
+      </l-tooltip>
+    </l-marker>
+
+    <l-polygon
+      color="#0099FF"
+      fillColor="#0099FF"
+      :lat-lngs="[
+        [50.0669, 19.9059],
+        [50.0638, 19.9010],
+        [50.0599, 19.9017],
+        [50.0566, 19.9005],
+        [50.0562, 19.9111],
+        [50.0568, 19.9216],
+        [50.0601, 19.9228],
+        [50.0641, 19.9242],
+        [50.0655, 19.9181],
+        [50.0657, 19.9105],
+      ]"
+    >
+      <l-tooltip>
+        Jeż
+      </l-tooltip>
+    </l-polygon>
+
+    <l-polygon
+      color="#008080"
+      fillColor="#008080"
+      :lat-lngs="[
+        [50.0591, 19.9098],
+        [50.0568, 19.8962],
+        [50.0524, 19.8992],
+        [50.0510, 19.9044],
+        [50.0513, 19.9119],
+        [50.0518, 19.9193],
+        [50.0531, 19.9250],
+        [50.0564, 19.9275],
+        [50.0573, 19.9228],
+        [50.0573, 19.9160],
+      ]"
+    >
+      <l-tooltip>
+        Sowa
+      </l-tooltip>
+    </l-polygon>
   </l-map>
 </template>
 
 <script>
-import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+import {
+  LMap,
+  LTileLayer,
+  LMarker,
+  LTooltip,
+  LPolygon,
+} from 'vue2-leaflet';
 import 'leaflet/dist/leaflet.css';
-// TODO fix
+
 import { Icon } from 'leaflet';
+import Api from '../axios/api';
+
 // eslint-disable-next-line
 delete Icon.Default.prototype._getIconUrl;
 Icon.Default.mergeOptions({
@@ -31,6 +90,8 @@ export default {
     LMap,
     LTileLayer,
     LMarker,
+    LTooltip,
+    LPolygon,
   },
   data: () => ({
     url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
@@ -38,7 +99,12 @@ export default {
       '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
     zoom: 12,
     center: [50.06143, 19.95],
-    markerLatLng: [50.06143, 19.980],
+    markers: [],
   }),
+  created() {
+    Api.get('/cities/1/boards').then(({ data }) => {
+      this.markers = data;
+    });
+  },
 };
 </script>
