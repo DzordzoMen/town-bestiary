b132c57d1be2ef6bd6db8ea761eaf09842333303
CI <CI@example.com>
2021-12-12 12:22:55 +0000

    deploy

---
+++ precache-manifest.c6e58be1cf76068864351663b62df0f4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43d719409fd00a0c888f",
+    "url": "/town-bestiary/css/app.35aba949.css"
+  },
+  {
+    "revision": "d7b3e6b390e1d68df1d1",
+    "url": "/town-bestiary/css/chunk-vendors.fada64b5.css"
+  },
+  {
+    "revision": "48a17d615827a11d96d3ef5faa5d4d77",
+    "url": "/town-bestiary/img/beaver-icon.48a17d61.png"
+  },
+  {
+    "revision": "ca642ef56aac48c1b5132278ed5c8ea1",
+    "url": "/town-bestiary/img/beaver-image.ca642ef5.jpg"
+  },
+  {
+    "revision": "7872b71717600fff50869865b0c73d26",
+    "url": "/town-bestiary/img/duck-icon.7872b717.png"
+  },
+  {
+    "revision": "564ecab4f851a80023a6355c39ed7ed2",
+    "url": "/town-bestiary/img/hedgehog-icon.564ecab4.png"
+  },
+  {
+    "revision": "74cf0b8360c45ead2c7fc68528ce682c",
+    "url": "/town-bestiary/img/hedgehog-image.74cf0b83.jpg"
+  },
+  {
+    "revision": "7edeeb4421fe68f99edb21bfe4e3662b",
+    "url": "/town-bestiary/img/logo2.7edeeb44.png"
+  },
+  {
+    "revision": "6521f8845e8fa1dd5a675dd98bdd9b50",
+    "url": "/town-bestiary/img/squirrel-icon.6521f884.png"
+  },
+  {
+    "revision": "4422d76c17b4201e5f1c10141b0322b8",
+    "url": "/town-bestiary/img/squirrel-image.4422d76c.jpg"
+  },
+  {
+    "revision": "bc8ade94ed4215e392c53d2f9d88f925",
+    "url": "/town-bestiary/index.html"
+  },
+  {
+    "revision": "43d719409fd00a0c888f",
+    "url": "/town-bestiary/js/app.5223d918.js"
+  },
+  {
+    "revision": "d7b3e6b390e1d68df1d1",
+    "url": "/town-bestiary/js/chunk-vendors.a559be43.js"
+  },
+  {
+    "revision": "9309f884dc7ca8d3a82d744daa28800c",
+    "url": "/town-bestiary/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/town-bestiary/robots.txt"
+  }
+]);
